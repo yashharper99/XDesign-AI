c113465b9edd251d76114eb1eb15655602122e3e
Yash Virani <yashharper@gmail.com>
2026-02-19 04:36:37 +0000

    Authentication Updated | CMT 8

--- app/(routes)/_common/header.tsx
+++ app/(routes)/_common/header.tsx
@@ -1,15 +1,20 @@
 "use client";
 
+import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
 import Logo from "@/components/logo";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
-import { LoginLink } from "@kinde-oss/kinde-auth-nextjs/components";
-import { MoonIcon, SunIcon } from "lucide-react";
+import { LoginLink, LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
+import { LogOutIcon, MoonIcon, SunIcon } from "lucide-react";
 import { useTheme } from "next-themes";
 import Link from "next/link";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { Avatar } from "@/components/ui/avatar";
+import { AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 const Header = () => {
     const { theme, setTheme } = useTheme();
+    const { user } = useKindeBrowserClient();
     const isDark = theme === "dark";
     return (
         <div className="sticky top-0 right-0 left-0 z-30">
@@ -50,9 +55,41 @@ const Header = () => {
                           )}
                         />
                     </Button>
+                    {user ? (
+                      <DropdownMenu>
+                        <DropdownMenuTrigger>
+                          <Avatar className="h-8 w-8 
+                          shrink-0 rounded-full">
+                            <AvatarImage
+                               src={user?.picture || ""}
+                               alt={user?.given_name || ""}
+                            />
+                            <AvatarFallback className="rounded-lg">
+                              {user?.given_name?.charAt(0)}
+                              {user?.family_name?.charAt(0)}
+                              </AvatarFallback>
+                          </Avatar>                            
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent className="w-56"
+                        align="end"
+                        >
+                          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                          <DropdownMenuSeparator />
+                          <DropdownMenuItem>
+                            <LogoutLink className="w-full flex items-center">
+                              <LogOutIcon className="size-4" />
+                               Logout                              
+                            </LogoutLink>
+                          </DropdownMenuItem>
+                        </DropdownMenuContent>
+                      </DropdownMenu>
+                    ) : (
+                    
                     <LoginLink>
                       <Button>Sign in</Button>
                     </LoginLink>
+
+                    )}
                  </div>
                 </div>
             </header>
---
+++ proxy.ts
@@ -0,0 +1,14 @@
+import { withAuth } from "@kinde-oss/kinde-auth-nextjs/middleware";
+
+export default withAuth(async function middleware(req) {}, {
+    // Middleware still runs on all routes, but doesn't protect the blog route
+    isReturnToCurrentPage: true,
+    publicPaths: ["/"],
+  }
+);
+
+export const config = {
+  matcher: [
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+  ],
+}
